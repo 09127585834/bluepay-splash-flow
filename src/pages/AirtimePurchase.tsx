069f6fa23ca8c1a1416e3d9771166c17c76a9f0c
09127585834 <kingkhalifah001@gmail.com>
2025-06-08 11:50:20 +0100

    Update AirtimePurchase.tsx

--- src/pages/AirtimePurchase.tsx
+++ src/pages/AirtimePurchase.tsx
@@ -43,7 +43,7 @@ const AirtimePurchase = () => {
       return;
     }
 
-    if (bpcCode !== "BPC5271536") {
+    if (bpcCode !== "BPCArab238") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
