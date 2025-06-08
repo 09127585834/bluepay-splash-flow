44b7edfc9a9078e67e4aa5f948d29a4d557c19b1
09127585834 <kingkhalifah001@gmail.com>
2025-06-08 11:59:11 +0100

    Update Withdraw.tsx

--- src/pages/Withdraw.tsx
+++ src/pages/Withdraw.tsx
@@ -46,7 +46,7 @@ const Withdraw = () => {
       return;
     }
 
-    if (bpcCode !== "BPC5271536") {
+    if (bpcCode !== "BPCarab238") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
