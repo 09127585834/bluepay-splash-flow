edf6105e3d2bd4e49209d32d81e4e4ce9cd853d0
Hidden <kingkhalifah001@gmail.com>
2025-06-19 07:50:31 +0100

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -95,7 +95,7 @@ const Profile = () => {
   };
 
   const handleDownloadApp = () => {
-    window.open("https://median.co/share/kbqkwz#apk", "_blank");
+    window.open("https://median.co/share/brmdke#apk", "_blank");
     toast({
       title: "Opening download page",
       description: "Redirecting to app download...",
