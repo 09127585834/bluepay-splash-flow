ac0ae8351380b86d2f5be0fbdf091f3253ecdb9f
Hidden <kingkhalifah001@gmail.com>
2025-06-19 07:45:23 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -116,12 +116,12 @@ const BuyBPCPayment = () => {
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">8973640269</p>
+            <p className="text-lg font-bold">3107376783</p>
             <Button
               variant="default"
               size="sm"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-1"
-              onClick={() => handleCopy("8973640269", "Account Number")}
+              onClick={() => handleCopy("3107376783", "Account Number")}
             >
               <Copy size={14} />
               Copy
@@ -131,12 +131,12 @@ const BuyBPCPayment = () => {
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Bank Name</p>
-          <p className="text-lg font-bold">PALMPAY LTD</p>
+          <p className="text-lg font-bold">POLARIS BANK</p>
         </div>
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Name</p>
-          <p className="text-lg font-bold">Khalifah Ibrahim</p>
+          <p className="text-lg font-bold">KHALIFAH IBRAHIM (Agent)</p>
         </div>
       </div>
 
